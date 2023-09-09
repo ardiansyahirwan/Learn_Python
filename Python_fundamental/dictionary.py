b75a64e0578688cd174fef56af8fd2f790888a61
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2023-09-10 00:57:53 +0700

    add file in python_fundamental

---
+++ Python_fundamental/dictionary.py
@@ -0,0 +1,20 @@
+dict_py = {
+   "name" : "irwan",
+   "age":17,
+   "Hobbies" : "coding",
+   "bool_1" : True,
+   "bool_2" : False
+}
+
+# # delete with specified keys
+# dict_py.pop("name")
+# print(dict_py)
+
+# # delete random last of dictionary
+# dict_py.popitem()
+# print(dict_py)
+# # for empties dictionary
+# print(dict_py.clear())
+# # for delete dictionary completely
+# del dict_py
+# print(dict_py)
