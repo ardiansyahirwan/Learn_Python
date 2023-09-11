91ec91839e50839d8898e6c5089120e7d2c4f64c
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2023-09-11 09:29:04 +0700

    add file to python_fundamental

---
+++ Python_fundamental/ternary.py
@@ -0,0 +1,6 @@
+# kita ubah nilai a nya menjadi False lalu kita run kembali
+a = False
+conditional = "a hasilnya True" if a else "a hasilnya False"
+print(conditional)
+
+# menggunakan ternary operator di python, kita dapat membuat conditional dalam satu baris
